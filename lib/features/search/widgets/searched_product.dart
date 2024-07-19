fc2941b1c3524490c56ba9ca70a13d7cda84e93a
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-19 14:56:47 +0530

    feat: Add SearchedProduct widget for displaying searched products

---
+++ lib/features/search/widgets/searched_product.dart
@@ -0,0 +1,85 @@
+import 'package:amazon_clone/common/widgets/stars.dart';
+import 'package:flutter/material.dart';
+
+import '../../../models/product.dart';
+
+class SearchedProduct extends StatelessWidget {
+  final Product product;
+  const SearchedProduct({
+    super.key,
+    required this.product,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              Image.network(
+                product.images[0],
+                fit: BoxFit.fitWidth,
+                height: 130,
+                width: 135,
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: Text(
+                      product.name,
+                      maxLines: 2,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: const Stars(
+                      rating: 4,
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: Text(
+                      "\$${product.price}",
+                      maxLines: 2,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: const Text(
+                      "Eligible for FREE Shipping",
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: const Text(
+                      "In Stock",
+                      maxLines: 2,
+                      style: TextStyle(
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
