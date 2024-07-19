924de63fab5d8618fe87b00796a16f231485e4cb
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-19 14:52:06 +0530

    chore: Add BottomBar widget for navigation

---
+++ lib/common/widgets/bottom_bar.dart
@@ -0,0 +1,75 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/account/screens/account_screen.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
+
+class BottomBar extends StatefulWidget {
+  static const String routeName = "/actual-home";
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _page = 0;
+  double bottomBarWidth = 42;
+  double bottomBarBorderWidth = 5;
+
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  List<Widget> pages = [
+    const HomeScreen(),
+    const AccountScreen(),
+    const Center(child: Text("Cart")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        selectedItemColor: GlobalVariables.selectedNavBarColor,
+        unselectedItemColor: GlobalVariables.unselectedNavBarColor,
+        backgroundColor: GlobalVariables.backgroundColor,
+        iconSize: 28,
+        onTap: updatePage,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline_outlined,
+            ),
+            label: "Profile",
+          ),
+          BottomNavigationBarItem(
+            icon: badges.Badge(
+              badgeContent: Text('3'),
+              badgeStyle: badges.BadgeStyle(
+                badgeColor: Colors.white,
+              ),
+              showBadge: true,
+              child: Icon(
+                Icons.shopping_cart_outlined,
+              ),
+            ),
+            label: "Cart",
+          ),
+        ],
+      ),
+    );
+  }
+}
