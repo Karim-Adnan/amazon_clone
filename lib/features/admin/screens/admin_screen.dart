2b406c22991560385d622d306d7daa6568806ece
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-19 14:53:51 +0530

    feat: Add AdminScreen widget for displaying admin interface

---
+++ lib/features/admin/screens/admin_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants/global_variables.dart';
+import 'products_screen.dart';
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({super.key});
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  int _page = 0;
+  double bottomBarWidth = 42;
+  double bottomBarBorderWidth = 5;
+
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  List<Widget> pages = [
+    const ProductsScreen(),
+    const Center(
+      child: Text('Analytics Page'),
+    ),
+    const Center(child: Text("Cart")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: GlobalVariables.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'assets/images/amazon_in.png',
+                  width: 120,
+                  height: 45,
+                  color: Colors.black,
+                ),
+              ),
+              const Text(
+                'Admin',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        selectedItemColor: GlobalVariables.selectedNavBarColor,
+        unselectedItemColor: GlobalVariables.unselectedNavBarColor,
+        backgroundColor: GlobalVariables.backgroundColor,
+        iconSize: 28,
+        onTap: updatePage,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+            ),
+            label: "Posts",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.analytics_outlined,
+            ),
+            label: "Analytics",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.all_inbox_outlined,
+            ),
+            label: "Orders",
+          ),
+        ],
+      ),
+    );
+  }
+}
