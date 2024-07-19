9f2255839126a225d7a9dc28537bda515ad5db3c
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-19 16:14:12 +0530

    chore: Update user model to include cart functionality

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const productSchema = require("../models/product");
+const { productSchema } = require("../models/product");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -37,6 +37,15 @@ const userSchema = mongoose.Schema({
     type: String,
     default: "user",
   },
+  cart: [
+    {
+      productSchema: productSchema,
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
