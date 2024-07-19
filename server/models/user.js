222541bf1d7319b9becfaf3ab150376baf360294
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-19 15:02:54 +0530

    feat: Add User model for representing user data

---
+++ server/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const productSchema = require("../models/product");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: (email) => {
+        const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        return email.match(re);
+      },
+      message: "Please enter a valid email address",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (password) => {
+        return password.length >= 6;
+      },
+      message: "Password must be at least 6 characters long",
+    },
+  },
+  address: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "user",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
