e150334fac46b465a468b3339e38b0c3f3aa69cb
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-19 14:51:59 +0530

    chore: Update router.dart with new screens and routes

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,13 @@
+import 'package:amazon_clone/common/widgets/bottom_bar.dart';
+import 'package:amazon_clone/features/home/screens/category_deals_screen.dart';
+import 'package:amazon_clone/features/product_details/screens/product_details_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'features/admin/screens/add_products_screen.dart';
 import 'features/auth/screens/auth_screen.dart';
+import 'features/home/screens/home_screen.dart';
+import 'features/search/screens/search_screen.dart';
+import 'models/product.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
   switch (routeSettings.name) {
@@ -9,6 +16,49 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
         settings: routeSettings,
         builder: (_) => const AuthScreen(),
       );
+
+    case HomeScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const HomeScreen(),
+      );
+
+    case BottomBar.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const BottomBar(),
+      );
+
+    case AddProductsScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const AddProductsScreen(),
+      );
+
+    case CategoryDealsScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => CategoryDealsScreen(
+          category: routeSettings.arguments as String,
+        ),
+      );
+
+    case SearchScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => SearchScreen(
+          searchQuery: routeSettings.arguments as String,
+        ),
+      );
+
+    case ProductDetailsScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => ProductDetailsScreen(
+          product: routeSettings.arguments as Product,
+        ),
+      );
+
     default:
       return MaterialPageRoute(
         builder: (_) => const Scaffold(
