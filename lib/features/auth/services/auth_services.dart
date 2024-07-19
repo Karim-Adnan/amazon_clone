f362d1176b1ef399338cde17f8592746a87d2c9e
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-19 14:54:29 +0530

    feat: Add AuthService class for handling user authentication and data retrieval

---
+++ lib/features/auth/services/auth_services.dart
@@ -0,0 +1,153 @@
+import 'dart:convert';
+
+import 'package:amazon_clone/common/widgets/bottom_bar.dart';
+import 'package:amazon_clone/constants/error_handling.dart';
+import 'package:amazon_clone/constants/utils.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
+import 'package:amazon_clone/models/user.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../constants/global_variables.dart';
+
+class AuthService {
+  void signUpUser({
+    required BuildContext context,
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      User user = User(
+        id: '',
+        name: name,
+        email: email,
+        password: password,
+        address: '',
+        type: '',
+        token: '',
+      );
+
+      Uri uri = Uri.http(baseUrl, '/api/signup');
+
+      http.Response res = await http.post(
+        uri,
+        body: user.toJson(),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackBar(
+            context: context,
+            message: 'Account created! Login with the same credentials!',
+          );
+        },
+      );
+    } catch (e) {
+      showSnackBar(
+        context: context,
+        message: e.toString(),
+      );
+    }
+  }
+
+  void signInUser({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      Uri uri = Uri.http(baseUrl, '/api/signin');
+
+      http.Response res = await http.post(
+        uri,
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+
+          Navigator.of(context).pushNamedAndRemoveUntil(
+            BottomBar.routeName,
+            (route) => false,
+          );
+          showSnackBar(
+            context: context,
+            message: 'Logged in successfully!',
+          );
+        },
+      );
+    } catch (e) {
+      print(e.toString());
+      showSnackBar(
+        context: context,
+        message: e.toString(),
+      );
+    }
+  }
+
+  void getUserData(
+    BuildContext context,
+  ) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('x-auth-token');
+
+      if (token == null) {
+        prefs.setString('x-auth-token', '');
+      }
+
+      Uri uri = Uri.http(baseUrl, '/api/tokenIsValid');
+
+      http.Response tokenRes = await http.post(
+        uri,
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token!,
+        },
+      );
+
+      var response = jsonDecode(tokenRes.body);
+
+      if (response == true) {
+        Uri userUri = Uri.http(baseUrl, '/');
+        http.Response userRes = await http.get(
+          userUri,
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'x-auth-token': token,
+          },
+        );
+
+        UserProvider userProvider =
+            Provider.of<UserProvider>(context, listen: false);
+
+        userProvider.setUser(userRes.body);
+      }
+    } catch (e) {
+      showSnackBar(
+        context: context,
+        message: e.toString(),
+      );
+    }
+  }
+}
