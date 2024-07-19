41cbfcf53a111ec48eec174e84f91f425de9bdb1
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-19 14:54:01 +0530

    feat: Add ProductsScreen widget for displaying products and managing them

---
+++ lib/features/admin/screens/products_screen.dart
@@ -0,0 +1,99 @@
+import 'package:amazon_clone/features/account/widgets/single_product.dart';
+import 'package:amazon_clone/features/admin/services/admin_services.dart';
+import 'package:amazon_clone/models/product.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/loader.dart';
+import 'add_products_screen.dart';
+
+class ProductsScreen extends StatefulWidget {
+  const ProductsScreen({super.key});
+
+  @override
+  State<ProductsScreen> createState() => _ProductsScreenState();
+}
+
+class _ProductsScreenState extends State<ProductsScreen> {
+  List<Product>? products;
+  final AdminServices adminServices = AdminServices();
+  @override
+  void initState() {
+    super.initState();
+    fetchAllProducts(context);
+  }
+
+  fetchAllProducts(BuildContext context) async {
+    products = await adminServices.fetchAllProducts(context: context);
+    setState(() {});
+  }
+
+  deleteProduct(Product product, int index) async {
+    adminServices.deleteProduct(
+      context: context,
+      product: product,
+      onSuccess: () {
+        products!.removeAt(index);
+        setState(() {});
+      },
+    );
+  }
+
+  void navigateToAddProduct() {
+    Navigator.of(context).pushNamed(AddProductsScreen.routeName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return products == null
+        ? const Loader()
+        : Scaffold(
+            body: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: products!.length,
+              itemBuilder: (context, index) {
+                final productData = products![index];
+
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 140,
+                      child: SingleProduct(
+                        image: productData.images[0],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            productData.name,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () => deleteProduct(productData, index),
+                          icon: const Icon(
+                            Icons.delete_outline,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                navigateToAddProduct();
+              },
+              tooltip: 'Add a Product',
+              child: const Icon(Icons.add),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
+  }
+}
