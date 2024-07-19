56cbdff72fd998ba7ea1bbbbd5aefd793081c8cb
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-19 14:52:28 +0530

    chore: Add CustomButton widget for reusable button component

---
+++ lib/common/widgets/custom_button.dart
@@ -0,0 +1,35 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+  final Color? color;
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        minimumSize: const Size(double.infinity, 50),
+        backgroundColor: color ?? GlobalVariables.secondaryColor,
+        foregroundColor: Colors.white,
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: color == null ? Colors.white : Colors.black,
+        ),
+      ),
+    );
+  }
+}
