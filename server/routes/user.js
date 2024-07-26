69d139d8a4f406c56647be82143b57c2d26449fc
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-26 16:55:45 +0530

    chore: Update user route to handle adding products to cart

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,43 @@
 const express = require("express");
-const adminRouter = express.Router();
+const userRouter = express.Router();
+const auth = require("../middlewares/auth");
+const { Product } = require("../models/product");
+const User = require("../models/user");
 
-adminRouter.get("", auth, async (req, res) => {
+userRouter.post("/api/add-to-cart", auth, async (req, res) => {
   try {
-    return res.status(200).json({});
+    const { id } = req.body;
+
+    const product = await Product.findById(id);
+
+    let user = await User.findById(req.user);
+
+    if (user.cart.length == 0) {
+      user.cart.push({ product, quantity: 1 });
+    } else {
+      let isProductFound = false;
+      for (let i = 0; i < user.cart.length; i++) {
+        if (user.cart[i].product._id.equals(product._id)) {
+          isProductFound = true;
+        }
+      }
+
+      if (isProductFound) {
+        let producttt = user.cart.find((productt) =>
+          productt.product._id.equals(product._id)
+        );
+
+        producttt.quantity += 1;
+      } else {
+        user.cart.push({ product, quantity: 1 });
+      }
+    }
+
+    user = await user.save();
+    return res.status(200).json(user);
   } catch (e) {
     return res.status(500).json({ error: e.message });
   }
 });
+
+module.exports = userRouter;
