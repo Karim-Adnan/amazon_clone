4abc7192264c708df1fa9420b81f06d2688d1c2f
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-26 16:56:21 +0530

    chore: Add CartProduct widget for displaying cart items

---
+++ lib/features/cart/widgets/cart_product.dart
@@ -0,0 +1,86 @@
+import 'package:amazon_clone/models/product.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CartProduct extends StatefulWidget {
+  final int index;
+  const CartProduct({
+    super.key,
+    required this.index,
+  });
+
+  @override
+  State<CartProduct> createState() => _CartProductState();
+}
+
+class _CartProductState extends State<CartProduct> {
+  @override
+  Widget build(BuildContext context) {
+    final cartProduct = context.watch<UserProvider>().user.cart[widget.index];
+    final Product product = Product.fromMap(cartProduct['product']);
+
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              Image.network(
+                product.images[0],
+                fit: BoxFit.fitWidth,
+                height: 130,
+                width: 135,
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: Text(
+                      product.name,
+                      maxLines: 2,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: Text(
+                      "\$${product.price}",
+                      maxLines: 2,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: const Text(
+                      "Eligible for FREE Shipping",
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: const Text(
+                      "In Stock",
+                      maxLines: 2,
+                      style: TextStyle(
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
