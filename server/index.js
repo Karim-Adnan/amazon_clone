569b194d6ff5109641831b4290eac8e73008f0a1
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-26 16:55:30 +0530

    feat: Add user route for managing user data

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/admin");
 const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
 
 // Initialize Express
 const PORT = 3000;
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(authRouter);
 app.use(adminRouter);
 app.use(productRouter);
+app.use(userRouter);
 
 // Connect to MongoDB
 mongoose
