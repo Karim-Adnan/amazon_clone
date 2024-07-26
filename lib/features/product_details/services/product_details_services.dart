aaa9bf18113a6a9bc1da3cbdf8b8543ab011610a
Adnan Karim <kaif.adnan05@gmail.com>
2024-07-26 16:56:43 +0530

    chore: Refactor addToCart method in ProductDetailsServices

--- lib/features/product_details/services/product_details_services.dart
+++ lib/features/product_details/services/product_details_services.dart
@@ -7,10 +7,53 @@ import 'package:provider/provider.dart';
 import '../../../constants/error_handling.dart';
 import '../../../constants/utils.dart';
 import '../../../models/product.dart';
+import '../../../models/user.dart';
 import '../../../providers/user_provider.dart';
 import '../../../constants/global_variables.dart';
 
 class ProductDetailsServices {
+  void addToCart({
+    required BuildContext context,
+    required Product product,
+  }) async {
+    final UserProvider userProvider =
+        Provider.of<UserProvider>(context, listen: false);
+    try {
+      Uri uri = Uri.http(baseUrl, '/api/add-to-cart');
+
+      http.Response res = await http.post(
+        uri,
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': userProvider.user.token,
+        },
+        body: jsonEncode(
+          {
+            "id": product.id!,
+          },
+        ),
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackBar(context: context, message: "Success");
+          User user = userProvider.user.copyWith(
+            cart: jsonDecode(res.body)['cart'],
+          );
+
+          userProvider.setUserFromModel(user);
+        },
+      );
+    } catch (e) {
+      showSnackBar(
+        context: context,
+        message: e.toString(),
+      );
+    }
+  }
+
   void rateProduct({
     required BuildContext context,
     required Product product,
