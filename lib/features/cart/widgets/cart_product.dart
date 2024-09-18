952198f974fcb734bf6be7a9152bfb9f705bb8e3
Adnan Karim <kaif.adnan05@gmail.com>
2024-09-18 19:45:23 +0530

    Refactor cart product quantity buttons in CartProduct widget

--- lib/features/cart/widgets/cart_product.dart
+++ lib/features/cart/widgets/cart_product.dart
@@ -1,7 +1,10 @@
+import 'package:amazon_clone/features/product_details/services/product_details_services.dart';
 import 'package:amazon_clone/models/product.dart';
 import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../services/cart_services.dart';
 
 class CartProduct extends StatefulWidget {
   final int index;
@@ -15,6 +18,19 @@ class CartProduct extends StatefulWidget {
 }
 
 class _CartProductState extends State<CartProduct> {
+  final ProductDetailsServices productDetailsServices =
+      ProductDetailsServices();
+
+  final CartServices cartServices = CartServices();
+
+  void increaseQuantity(Product product) {
+    productDetailsServices.addToCart(context: context, product: product);
+  }
+
+  void decreaseQuantity(Product product) {
+    cartServices.removeFromCart(context: context, product: product);
+  }
+
   @override
   Widget build(BuildContext context) {
     final cartProduct = context.watch<UserProvider>().user.cart[widget.index];
@@ -76,7 +92,50 @@ class _CartProductState extends State<CartProduct> {
                     ),
                   ),
                 ],
-              )
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () => decreaseQuantity(product),
+                      child: const Icon(
+                        Icons.remove,
+                        size: 20,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      cartProduct['quantity'].toString(),
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    InkWell(
+                      onTap: () => increaseQuantity(product),
+                      child: const Icon(
+                        Icons.add,
+                        size: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
